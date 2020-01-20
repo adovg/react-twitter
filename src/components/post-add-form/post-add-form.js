c4c94b8bfb710bf11ee513a1f4ba5590dccdbc31
adovg <you@example.com>
2020-01-20 16:41:12 +0200

    initial commit

---
+++ src/components/post-add-form/post-add-form.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './post-add-form.css';
+
+const PostAddForm = () => {
+    return (
+        <form className="bottom-pannel d-flex">
+            <input type="text" placeholder="о чем вы думаете сейчас" className="form-control new-post-label" />
+            <button type="submit"
+                    className="btn btn-outline-secondary">
+                        Добавить
+            </button>
+        </form>
+    )
+};
+
+export default PostAddForm;
---
+++ src/components/post-status-filter/post-status-filter.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './post-status-filter.css';
+
+const PostStatusFilter = () => {
+    return (
+        <div className="btn-group">
+            <button  type="submit"
+                    className="btn btn-outline-secondary"></button>
+            <button  type="submit"
+                    className="btn btn-outline-secondary"></button>                  
+            {/* <button/>
+            <button/> */}
+        </div>
+    )
+}
+
+export default PostStatusFilter;
